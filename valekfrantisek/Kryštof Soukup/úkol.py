b436b3edd7b315ae6d90d27ad4d1ee2432bf127f
KrystofSoukup <st72063@upce.cz>
2023-10-25 11:17:57 +0200

    fttftft

---
+++ valekfrantisek/Kryštof Soukup/úkol.py
@@ -0,0 +1,13 @@
+is_male = True
+fešák = False
+if is_male and fešák:
+    print("Jseš chlap a fešák")
+elif is_male and not fešák:
+    print("Jseš chlap, ale nejsi fešák")
+elif not is_male and fešák:
+    print("Nejsi chlap, ale jseš fešák")
+else:
+    print ("Nejsi ani chlap, ani fešák.")
+
+for letter in "Willem Dafoe":
+    print ("je to super herec")
