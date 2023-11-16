c9add7224852276feb1897819908169d748a5922
František Válek <55525391+valekfrantisek@users.noreply.github.com>
2023-11-16 15:56:28 +0100

    upload_test

---
+++ valekfrantisek/speed_test_2.py
@@ -0,0 +1,44 @@
+import time
+
+start = time.time()
+
+for i in range(100):
+    for b in range(1000000):
+        pass
+
+end = time.time()
+
+print(end - start)
+
+start = time.time()
+
+for i in range(100):
+    for b in range(1000000):
+        pass
+
+end = time.time()
+
+print(end - start)
+
+start = time.time()
+
+for i in range(100):
+    for b in range(1000000):
+        pass
+
+end = time.time()
+
+print(end - start)
+
+start = time.time()
+
+for i in range(100):
+    for b in range(1000000):
+        pass
+
+end = time.time()
+
+print(end - start)
+
+# This is an issue
+print('issue')
