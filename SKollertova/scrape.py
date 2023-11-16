6fe0ca246b31a0d793653e50fd0733cdfbfda67a
valekfrantisek <valekfrantisek@gmail.com>
2023-11-16 13:36:22 +0100

    scr

--- SKollertova/scrape.py
+++ SKollertova/scrape.py
@@ -40,61 +40,61 @@ for i in range(1,51):
     is_product = False
     for line in web_lines:
         if '<article class="product_pod">' in line:
-    is_product = True
-    if '<img src="' in line and is_product:
-        start = line.find('<img src="')
-        end = line.find('" alt="')
-        img_url = line[start + 10:end]
-        images_url.append(img_url)
+        is_product = True
+        if '<img src="' in line and is_product:
+            start = line.find('<img src="')
+            end = line.find('" alt="')
+            img_url = line[start + 10:end]
+            images_url.append(img_url)
+            is_product = False
+
+        print(images_url)
+        print(len(images_url))
+
+        for img_url in images_url:
+        print(url + img_url)
+
+        img_filename = img_url.split('/')[-1]
+
+        response = requests.get(url + img_url)
+
+        with open(os.path.join(SCRAPE_PATH, img_filename), 'wb') as out_file:
+            out_file.write(response.content)
+            print('\t', img_filename, 'downloaded')
+
+        time.sleep(2)
+
+        web_lines = text.splitlines()
+        images_url_et_names = {}
+
         is_product = False
 
-    print(images_url)
-    print(len(images_url))
+        for i, line in enumerate(web_lines):
+            if '<article class="product_pod">' in line:
+                is_product = True
+            if '<img src="' in line and is_product:
+                start = line.find('<img src="')
+                end = line.find('" alt="')
+                img_url = line[start + 10:end]
 
-    for img_url in images_url:
-    print(url + img_url)
+                start = line.find('" alt="')
+                end = line.find('" class="thumbnail"')
+                img_name = line[start + 7:end]
 
-    img_filename = img_url.split('/')[-1]
+                images_url_et_names[i] = [img_url, img_name]
+                is_product = False
 
-    response = requests.get(url + img_url)
+        print('Urls and names retrieved.')
 
-    with open(os.path.join(SCRAPE_PATH, img_filename), 'wb') as out_file:
-        out_file.write(response.content)
-        print('\t', img_filename, 'downloaded')
+        for i in images_url_et_names:
+            print(i, images_url_et_names[i])
+            img_url = images_url_et_names[i][0]
+            img_name = images_url_et_names[i][1]
 
-    time.sleep(2)
+            response = requests.get(url + img_url)
 
-    web_lines = text.splitlines()
-    images_url_et_names = {}
+            with open(os.path.join(SCRAPE_PATH, f'{img_name}.jpg'), 'wb') as out_file:
+                out_file.write(response.content)
+                print('\t', img_name, 'downloaded')
 
-    is_product = False
-
-    for i, line in enumerate(web_lines):
-        if '<article class="product_pod">' in line:
-    is_product = True
-    if '<img src="' in line and is_product:
-        start = line.find('<img src="')
-        end = line.find('" alt="')
-        img_url = line[start + 10:end]
-
-        start = line.find('" alt="')
-        end = line.find('" class="thumbnail"')
-        img_name = line[start + 7:end]
-
-        images_url_et_names[i] = [img_url, img_name]
-        is_product = False
-
-    print('Urls and names retrieved.')
-
-    for i in images_url_et_names:
-        print(i, images_url_et_names[i])
-    img_url = images_url_et_names[i][0]
-    img_name = images_url_et_names[i][1]
-
-    response = requests.get(url + img_url)
-
-    with open(os.path.join(SCRAPE_PATH, f'{img_name}.jpg'), 'wb') as out_file:
-        out_file.write(response.content)
-        print('\t', img_name, 'downloaded')
-
-    time.sleep(2)
+            time.sleep(2)
