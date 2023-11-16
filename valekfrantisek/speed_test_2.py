15f1e5f39b693cc530cee5dfe8047f4f7e7f178c
valekfrantisek <valekfrantisek@gmail.com>
2023-11-16 16:05:39 +0100

    test

---
+++ valekfrantisek/speed_test_2.py
@@ -0,0 +1,45 @@
+import time
+
+start = time.time()
+
+for i in range(100):
+    for b in range(1000000):
+        pass
+
+end = time.time()
+
+print(end - start)
+
+start = time.time()
+
+for i in range(100):
+    for b in range(1000000):
+        pass
+
+end = time.time()
+
+print(end - start)
+
+start = time.time()
+
+for i in range(100):
+    for b in range(1000000):
+        pass
+
+end = time.time()
+
+print(end - start)
+
+start = time.time()
+
+for i in range(100):
+    for b in range(1000000):
+        pass
+
+end = time.time()
+
+print(end - start)
+
+# This is an issue
+print('issue')
+print('another issue')
